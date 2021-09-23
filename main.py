0b6a4447098bfe2a6c9d977c3f359286b9b5dadd
imlei <huanglei@live.com>
2021-09-23 00:36:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #对Amazon的GST和PST的信息进行汇总合并，并生成报表。
+#自动读取目录的文件，并转换成Xlsx文件，此外输出最终的结果
 
 import os
 import csv
 import numpy as np
 import pandas as pd
+import openpyxl
 
+#filename = os.path.abspath('.')
+#print(filename)
+#print(os.path.abspath("__file__"))
+#print(os.path.abspath(filename))
 #参数设置
 #设置转为为Excel的文件名
 set_excel_file = "taxreport.xlsx"
@@ -20,6 +26,7 @@ filePath = r"D:\test\taxfile"
 os.chdir(filePath)
 print("文件夹的地址")
 print(filePath)
+#遍历设置好的目录，并且找到CSV文件
 print("开始遍历")
 filecsv = []
 for root, dirs, files in os.walk(filePath):
@@ -37,6 +44,6 @@ for csv_file in filecsv:
     excelfile = csvtoxlsx(csv_file)
 
 #读取和操作Excel的文件
-dfs = pd.read_excel(set_excel_file)
-taxreport = pd.pivot_table(dfs, values="Tax_Amount", index="Tax_Type", aggfunc="sum", margins=True, margins_name="合计")
-print(taxreport)
+df = pd.read_excel(set_excel_file)
+taxreport = pd.pivot_table(df, values=["Tax_Amount","Taxable_Amount"], index="Tax_Type", aggfunc="sum", margins=True, margins_name="Total")
+taxreport.to_excel("taxsum.xlsx")
